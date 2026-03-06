b830ef78ccfc94db15e357cf2e7d7f3e8bf57583
Leg_Day <mosaabelbanhawy@gmail.com>
2026-03-06 19:19:42 +0200

    Update ExamplePlugin.java

--- src/main/java/com/crimsonwarpedcraft/exampleplugin/ExamplePlugin.java
+++ src/main/java/com/crimsonwarpedcraft/exampleplugin/ExamplePlugin.java
@@ -1,19 +1,45 @@
 package com.crimsonwarpedcraft.exampleplugin;
 
-import io.papermc.lib.PaperLib;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * Created by Levi Muniz on 7/29/20.
- *
- * @author Copyright (c) Levi Muniz. All Rights Reserved.
- */
-public class ExamplePlugin extends JavaPlugin {
+public class SoundSwapPlugin extends JavaPlugin implements Listener {
 
-  @Override
-  public void onEnable() {
-    PaperLib.suggestPaper(this);
+    private FileConfiguration config;
 
-    saveDefaultConfig();
-  }
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        config = getConfig();
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getLogger().info("SoundSwapPlugin Enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("SoundSwapPlugin Disabled!");
+    }
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
+        if (event.getEntityType() == EntityType.VILLAGER) {
+            // اقرأ الصوت البديل من config
+            String soundName = config.getString("villager-sound", "ENTITY_SHEEP_AMBIENT");
+            float pitch = (float) config.getDouble("pitch", 1.0);
+            float volume = (float) config.getDouble("volume", 1.0);
+
+            try {
+                Sound sound = Sound.valueOf(soundName);
+                event.getEntity().getWorld().playSound(event.getEntity().getLocation(), sound, volume, pitch);
+            } catch (IllegalArgumentException e) {
+                getLogger().warning("Sound not found: " + soundName);
+            }
+        }
+    }
 }
